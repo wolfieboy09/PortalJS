16ba5bc80d1437cd53b8ec18abe7507b0e5ea6ed
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-27 08:33:47 -0500

    I was registering it wrong the entire time :sob:

--- src/main/java/dev/wolfieboy09/portaljs/BlockRegistry.java
+++ src/main/java/dev/wolfieboy09/portaljs/BlockRegistry.java
@@ -2,11 +2,16 @@ package dev.wolfieboy09.portaljs;
 
 import dev.wolfieboy09.portaljs.blocks.PJSPortalBlock;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class BlockRegistry {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PortalJS.MODID);
 
-    public static final DeferredBlock<PJSPortalBlock> PORTAL_BLOCK = DeferredBlock.createBlock(ResourceLocation.fromNamespaceAndPath(PortalJS.MODID, "pjs_portal_block"));
+    public static final DeferredBlock<PJSPortalBlock> PORTAL_BLOCK = BLOCKS.register("portal_block", registryName -> new PJSPortalBlock(null));
+
+    public static void register(IEventBus bus) {
+        BLOCKS.register(bus);
+    }
 }
