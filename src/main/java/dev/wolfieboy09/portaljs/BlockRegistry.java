4330bab61f96fa6cc9b7c8c443665ab0006ec932
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-02-27 14:50:20 -0500

    remove import

--- src/main/java/dev/wolfieboy09/portaljs/BlockRegistry.java
+++ src/main/java/dev/wolfieboy09/portaljs/BlockRegistry.java
@@ -1,13 +1,12 @@
 package dev.wolfieboy09.portaljs;
 
 import dev.wolfieboy09.portaljs.blocks.PJSPortalBlock;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class BlockRegistry {
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PortalJS.MODID);
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PortalJS.MOD_ID);
 
     public static final DeferredBlock<PJSPortalBlock> PORTAL_BLOCK = BLOCKS.register("portal_block", registryName -> new PJSPortalBlock(null));
 
