e84bc2a748f1b0b9d2425836761c0de4c7916da3
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-05 12:01:38 -0500

    Use ServerLevel over Level

--- src/main/java/dev/wolfieboy09/portaljs/context/PortalBlockContext.java
+++ src/main/java/dev/wolfieboy09/portaljs/context/PortalBlockContext.java
@@ -2,12 +2,11 @@ package dev.wolfieboy09.portaljs.context;
 
 import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 public record PortalBlockContext(@Info("The block state of the portal") BlockState state,
-                                 @Info("The level where the portal exists") Level level,
+                                 @Info("The level where the portal exists") ServerLevel level,
                                  @Info("The position of the portal block") BlockPos pos,
-                                 @Info("The random source used for effects or behavior") RandomSource randomSource) {
-}
+                                 @Info("The random source used for effects or behavior") RandomSource randomSource) {}
