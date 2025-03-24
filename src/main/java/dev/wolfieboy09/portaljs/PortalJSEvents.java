d1da756676ab209b84379fe9e563bad7d0a0e828
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-24 19:53:56 -0400

    More event stuff of portals

--- src/main/java/dev/wolfieboy09/portaljs/PortalJSEvents.java
+++ src/main/java/dev/wolfieboy09/portaljs/PortalJSEvents.java
@@ -4,6 +4,7 @@ import dev.wolfieboy09.portaljs.events.CreatedPortalEvent;
 import dev.wolfieboy09.portaljs.kubeevents.PortalEvents;
 import dev.wolfieboy09.portaljs.kubeevents.PortalJSSpawnEvent;
 import net.kyrptonaught.customportalapi.util.CustomPortalHelper;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.event.level.BlockEvent;
@@ -12,17 +13,21 @@ import org.jetbrains.annotations.NotNull;
 public class PortalJSEvents {
     public static void vanillaPortalSpawn(@NotNull BlockEvent.PortalSpawnEvent event) {
         // This event only gets fired from nether portals... so I could just... hard code it...
-        event.setCanceled(sendEvent(Blocks.OBSIDIAN));
+        event.setCanceled(sendEvent(Blocks.OBSIDIAN, event.getPos()));
     }
 
     public static void customPortalSpawn(@NotNull CreatedPortalEvent event) {
-        Block frameblock = CustomPortalHelper.getPortalBase(event.getLevel(), event.getPos());
-        event.setCanceled(sendEvent(frameblock));
+        if (!event.isVanilla) {
+            event.setCanceled(sendEvent(CustomPortalHelper.getPortalBase(event.getLevel(), event.getPos().below()), event.getPos()));
+        } else {
+            event.setCanceled(sendEvent(event.getFoundationBlock(), event.getPos()));
+        }
     }
 
-    private static boolean sendEvent(Block foundation) {
+    private static boolean sendEvent(Block foundation, BlockPos blockPos) {
         if (PortalEvents.PORTAL_SPAWN.hasListeners()) {
-            PortalJSSpawnEvent portalSpawnEvent = new PortalJSSpawnEvent(foundation);
+            // The block pos where the portal was ignited
+            PortalJSSpawnEvent portalSpawnEvent = new PortalJSSpawnEvent(foundation, blockPos);
             PortalEvents.PORTAL_SPAWN.post(portalSpawnEvent);
             return portalSpawnEvent.isCancelled();
         }
