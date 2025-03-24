264aea03e0e0964ddff35a48c134e4b4639918ed
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-24 19:54:13 -0400

    Add block pos for the portal spawn context

--- src/main/java/dev/wolfieboy09/portaljs/kubeevents/PortalJSSpawnEvent.java
+++ src/main/java/dev/wolfieboy09/portaljs/kubeevents/PortalJSSpawnEvent.java
@@ -2,15 +2,19 @@ package dev.wolfieboy09.portaljs.kubeevents;
 
 import dev.latvian.mods.kubejs.event.KubeEvent;
 import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 
 public class PortalJSSpawnEvent implements KubeEvent {
     public final Block frameBlock;
+    public final BlockPos blockPos;
+
     @HideFromJS
     private boolean isCancelled = false;
 
-    public PortalJSSpawnEvent(Block frameBlock) {
+    public PortalJSSpawnEvent(Block frameBlock, BlockPos blockPos) {
         this.frameBlock = frameBlock;
+        this.blockPos = blockPos;
     }
 
     public void cancel() {
