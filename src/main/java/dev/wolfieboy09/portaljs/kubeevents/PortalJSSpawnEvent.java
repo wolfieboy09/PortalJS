b3d39ac775188291f2df246a3a9cd5607e1795ad
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-25 09:44:43 -0400

    Add server level to portal spawn event

--- src/main/java/dev/wolfieboy09/portaljs/kubeevents/PortalJSSpawnEvent.java
+++ src/main/java/dev/wolfieboy09/portaljs/kubeevents/PortalJSSpawnEvent.java
@@ -3,6 +3,7 @@ package dev.wolfieboy09.portaljs.kubeevents;
 import dev.latvian.mods.kubejs.event.KubeEvent;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Block;
 
 public class PortalJSSpawnEvent implements KubeEvent {
@@ -10,11 +11,18 @@ public class PortalJSSpawnEvent implements KubeEvent {
     public final BlockPos blockPos;
 
     @HideFromJS
+    private final ServerLevel serverLevel;
+    @HideFromJS
     private boolean isCancelled = false;
 
-    public PortalJSSpawnEvent(Block frameBlock, BlockPos blockPos) {
+    public PortalJSSpawnEvent(Block frameBlock, BlockPos blockPos, ServerLevel server) {
         this.frameBlock = frameBlock;
         this.blockPos = blockPos;
+        this.serverLevel = server;
+    }
+
+    public ServerLevel getServer() {
+        return this.serverLevel;
     }
 
     public void cancel() {
