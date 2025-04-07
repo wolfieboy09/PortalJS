8aea4d766d20a6d0689c3b97a4c4e40e1ee76e36
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-04-07 09:02:58 -0400

    Recordify

--- src/main/java/dev/wolfieboy09/portaljs/api/BiHolder.java
+++ src/main/java/dev/wolfieboy09/portaljs/api/BiHolder.java
@@ -1,19 +1,3 @@
 package dev.wolfieboy09.portaljs.api;
 
-public class BiHolder<L, R> {
-    private final L left;
-    private final R right;
-
-    public BiHolder(L left, R right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public L getLeft() {
-        return this.left;
-    }
-
-    public R getRight() {
-        return this.right;
-    }
-}
+public record BiHolder<L, R>(L left, R right) { }
